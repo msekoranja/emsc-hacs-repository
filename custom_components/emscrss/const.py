345dd6c4412767cc132aedfe7e89d90ddae0fb45
Matej Sekoranja <matej.sekoranja@gmail.com>
2021-01-01 12:58:50 +0100

    initial changes

--- custom_components/emscrss/const.py
+++ custom_components/emscrss/const.py
@@ -1,29 +1,21 @@
-"""Constants for integration_blueprint."""
+"""Constants for emscrss."""
 # Base component constants
-NAME = "Integration blueprint"
-DOMAIN = "integration_blueprint"
+NAME = "EMSC Earthquake RSS Feed"
+DOMAIN = "emscrss"
 DOMAIN_DATA = f"{DOMAIN}_data"
 VERSION = "0.0.1"
-ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
-ISSUE_URL = "https://github.com/custom-components/integration_blueprint/issues"
+ATTRIBUTION = "Data provided by https://www.emsc-csem.org/service/rss/"
+ISSUE_URL = "https://github.com/msekoranja/emsc-hacs-repository/issues"
 
 # Icons
 ICON = "mdi:format-quote-close"
 
-# Device classes
-BINARY_SENSOR_DEVICE_CLASS = "connectivity"
-
 # Platforms
-BINARY_SENSOR = "binary_sensor"
 SENSOR = "sensor"
-SWITCH = "switch"
-PLATFORMS = [BINARY_SENSOR, SENSOR, SWITCH]
+PLATFORMS = [SENSOR]
 
 
 # Configuration and options
-CONF_ENABLED = "enabled"
-CONF_USERNAME = "username"
-CONF_PASSWORD = "password"
 
 # Defaults
 DEFAULT_NAME = DOMAIN
